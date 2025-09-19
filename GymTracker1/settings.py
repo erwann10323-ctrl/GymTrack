e6d4215cdc184438342c8531e33c7330bff2ee41
erwann10323-ctrl <erwann10323@g.lfis.edu.hk>
2025-09-19 12:49:04 +0800

    Update settings.py

--- GymTracker1/settings.py
+++ GymTracker1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ci%-_u101(v(kdm)c9zo4g$mbjkdmz+@3f$ioht^1-kj+!p-%-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['GymTrack1.pythonanywhere.com']
 
 
 # Application definition
@@ -123,8 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
